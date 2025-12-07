0f2f77e3fb536fc3fd09fd2ce0fb468ce8531ed7
AuxiliumCDNG <soenke.klock@outlook.de>
2025-12-07 16:08:05 +0100

    added measure station auth

--- backend/measure_station.py
+++ backend/measure_station.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException
+from starlette import status
 
 from hardware import dht22
+
+load_dotenv()
 
 app = FastAPI()
 
 @app.get("/get/")
-async def temperature():
+async def temperature(auth: str):
+    if auth != os.environ["MEASURE_STATION_AUTHENTICATION"]:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
     temp_i, humid_i = await dht22.get_data_indoor()
     temp_o, humid_o = await dht22.get_data_outdoor()
 
