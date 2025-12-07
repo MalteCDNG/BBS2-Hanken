abc084a80060c0be6e8d7c87370c4dab467419ca
AuxiliumCDNG <soenke.klock@outlook.de>
2025-12-07 16:10:53 +0100

    add lifecycle management for hardware

--- backend/dependencies/app.py
+++ backend/dependencies/app.py
@@ -4,16 +4,34 @@ from beanie import init_beanie
 from fastapi import FastAPI, WebSocket
 from fastapi_crons import Crons, get_cron_router
 
+import dependencies.globals
+import hardware.util
 from dependencies.db import database
-from dependencies.models import Reading
+from dependencies.models import Reading, State, Settings
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    await init_beanie(database=database, document_models=[Reading])
-    yield
-    # TODO: Add function to run on shutdown to clear hardware stuff
+async def lifespan(fastapi_app: FastAPI):
+    await init_beanie(database=database, document_models=[Reading, State, Settings])
+    db_settings = await Settings.find_all().to_list()
 
+    if len(db_settings) == 0:
+        new_settings = Settings(
+            dht22_indoor_address="",
+            dht22_outdoor_address="",
+            data_cron="*/30 * * * *"
+        )
+        await new_settings.insert()
+        dependencies.globals.settings = new_settings
+    if len(db_settings) == 1:
+        print("populating settings")
+        print(db_settings[0])
+        dependencies.globals.settings = db_settings[0]
+    if len(db_settings) > 1:
+        raise RuntimeError("Settings could not be determined. Amount Settings: "+str(len(db_settings)))
+
+    yield
+    hardware.util.shutdown()
 
 app = FastAPI(lifespan=lifespan)
 crons = Crons(app)
