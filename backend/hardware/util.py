22917baa6a9c82e514280a87d334bf8c3bf68d4d
AuxiliumCDNG <soenke.klock@outlook.de>
2025-12-07 16:10:16 +0100

    added functions for hardware init and shutdown

--- backend/hardware/util.py
+++ backend/hardware/util.py
@@ -1,8 +1,19 @@
 import io
 
+from dependencies.models import State
+
+
 def is_raspberrypi():
     try:
         with io.open('/sys/firmware/devicetree/base/model', 'r') as m:
             if 'raspberry pi' in m.read().lower(): return True
     except Exception: pass
     return False
+
+def sync_state(state: State):
+    # TODO: implement function to sync new state to hardware
+    print("Syncing state to hardware.")
+
+def shutdown():
+    # TODO: Add function to run on shutdown to clear hardware stuff
+    print("Shutting down hardware")
