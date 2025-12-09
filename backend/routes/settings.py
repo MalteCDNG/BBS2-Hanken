f75673c6a9d13510bc3c1d9555c897857b31f71e
AuxiliumCDNG <soenke.klock@outlook.de>
2025-12-09 13:49:20 +0100

    add authentication to settings endpoints

--- backend/routes/settings.py
+++ backend/routes/settings.py
@@ -1,19 +1,22 @@
+from typing import Annotated
+
 import validators
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
 
 import dependencies.app
 import dependencies.globals
 from dependencies.models import Settings
+from routes.auth import User, get_current_active_user
 
 router = APIRouter()
 
 @router.get("/")
-async def get_settings() -> Settings:
+async def get_settings(current_user: Annotated[User, Depends(get_current_active_user)]) -> Settings:
     from_db = await Settings.find_one()
     return from_db
 
 @router.post("/")
-async def update_settings(settings: Settings):
+async def update_settings(settings: Settings, current_user: Annotated[User, Depends(get_current_active_user)]):
     from_db = await Settings.find_one()
     settings.id = from_db.id
     await settings.save()
@@ -27,7 +30,7 @@ async def update_settings(settings: Settings):
 ############################ Separate endpoints for changing station URLs ############################
 
 @router.post("/dht22_indoor_address/")
-async def set_dht22_indoor_address(address: str):
+async def set_dht22_indoor_address(address: str, current_user: Annotated[User, Depends(get_current_active_user)]):
     if not validators.url(address):
         raise HTTPException(
             status_code=400,
@@ -41,7 +44,7 @@ async def set_dht22_indoor_address(address: str):
     return "ok"
 
 @router.post("/dht22_outdoor_address/")
-async def set_dht22_outdoor_address(address: str):
+async def set_dht22_outdoor_address(address: str, current_user: Annotated[User, Depends(get_current_active_user)]):
     if not validators.url(address):
         raise HTTPException(
             status_code=400,
